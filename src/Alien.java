d318ac5517246142689b4ebbbc3883941856423d
basharalsaneh <83920082+basharalsaneh@users.noreply.github.com>
2021-05-23 14:07:19 +0200

    Lade till Agent inloggning

--- src/Alien.java
+++ src/Alien.java
@@ -8,12 +8,12 @@
  *
  * @author macbook
  */
-public class Alian extends javax.swing.JFrame {
+public class Alien extends javax.swing.JFrame {
 
     /**
      * Creates new form Alian
      */
-    public Alian() {
+    public Alien() {
         initComponents();
     }
 
@@ -120,20 +120,21 @@ public class Alian extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Alian.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Alien.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Alian.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Alien.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Alian.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Alien.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Alian.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Alien.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Alian().setVisible(true);
+                new Alien().setVisible(true);
             }
         });
     }
